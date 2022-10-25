63fbbfed284ca452bd2b4a7417a778da518dc613
w9561486 <w9561486@live.tees.ac.uk>
2022-10-25 10:21:47 +0100

    First commit

---
+++ app/src/main/java/uk/ac/tees/mad/w9561486/mylab/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.mad.w9561486.mylab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
